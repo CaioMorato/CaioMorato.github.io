0cf89e9cffc8a22652384abaebb3e90ceba2c32c
Caio Morato <morato_dev@protonmail.com>
2021-10-30 13:48:18 -0300

    recomeçar

--- src/App.js
+++ src/App.js
@@ -1,17 +1,11 @@
 // components
-import Header from './components/Header';
-import About from './components/About';
 // styles
 import './styles.css';
 import './breakpoints.css';
-import Skills from './components/Skills';
 
 function App() {
   return (
     <main>
-      <Header />
-      <About />
-      <Skills />
     </main>
   );
 }
