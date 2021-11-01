10768724d3ca52b1ef3bcbc2cdff213b758d4c80
Caio Morato <morato_dev@protonmail.com>
2021-11-01 18:51:33 -0300

    mais alguns ajustes de media queries e pretendo adicionar uma nova seção de habilidades para monitores maiores

--- src/pages/About.js
+++ src/pages/About.js
@@ -10,12 +10,27 @@ function About() {
         <div className="image-container">
           <img src={pfp} alt="Minha foto" />
         </div>
-        <div className="p-container">
-          <p>
-            Me chamo Caio Morato, tenho 23 e moro em Piracicaba-SP. Comecei meus estudos em
-            programação no começo de 2021. Sou um desenvolvedor fullstack com foco em back-end e
-            estou em busca de minha primeira oportunidade no mercado de trabalho.
-          </p>
+        <div className="p-skills-container">
+          <div className="p-container">
+            <p>
+              Me chamo Caio Morato, tenho 23 e moro em Piracicaba-SP. Comecei meus estudos em
+              programação no começo de 2021. Sou um desenvolvedor fullstack com foco em back-end e
+              estou em busca de minha primeira oportunidade no mercado de trabalho.
+            </p>
+          </div>
+          <div className="skills-container">
+            <h2>Habilidades</h2>
+            <ul>
+              <li>React</li>
+              <li>Redux</li>
+              <li>Bootstrap</li>
+              <li>Jest | RTL</li>
+              <li>Node</li>
+              <li>MongoDB</li>
+              <li>SQL</li>
+              <li>Express</li>
+            </ul>
+          </div>
         </div>
         <div className="cv-container">
           <a href={cv} className="cv-button" download>
